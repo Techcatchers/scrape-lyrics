533a38d3779aa513b25ea2bd35be21a9b14d14d7
Rishabh Agrawal <rishabha.1999@gmail.com>
2018-10-23 16:38:49 +0530

    Initial commit

---
+++ ReadMe.md
@@ -0,0 +1,18 @@
+# Web scraping lyrics with Genius and Musixmatch
+Get lyrics of a song by just passing in the song name. There are 2 methods used to find the lyrics for a song so if 1 method fails to find the lyrics for the song then another methods finds it and returns the well-formatted lyrics ready for educational use. But if both the method fails than it returns 'No lyrics found for the song.'
+
+### Requirements:
+* Python installed on your machine
+* Selenium Library installed
+* ChromeDriver or Firefox installed for browser Automation
+
+<br>
+
+### Copyright Information:
+I have uploaded this project for educational use only. Any commercial use of the scraped content may be prohibited by these sites so please read their terms and policies before using them anywhere other than educational uses as Web Scraping may not be allowed by Genius.com and Musixmatch.com. I shall not be liable for any misuse of this project as I have mentioned everything clearly and if you have any doubts about the scraped content usage, please contact the respective site owners.  
+
+#### You are All set to fork this Repository and modify it as per your needs.
+
+#### And Yes, if you like this Repository, then please star this repository as it motivates me to upload more Open Source projects like this.
+
+#### Get Set GO!
---
+++ get_lyrics.py
@@ -0,0 +1,88 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from xml.dom import minidom
+import requests
+from bs4 import BeautifulSoup
+
+
+def lyrics_generator(song_name):
+    try:
+        # Method 1 Genius.com
+        options = webdriver.ChromeOptions()
+        options.add_argument('headless')
+        options.add_argument('window-size=1200x600')
+
+        browser = webdriver.Chrome(
+            r'C:\Users\Rishabh-Pc\Downloads\chromedriver', chrome_options=options)
+
+        s = "https://www.google.co.in/search?q=" + song_name + "%20genius%20lyrics"
+        # s = "https://genius.com/Hailee-steinfeld-and-alesso-let-me-go-lyrics"
+        browser.get(s)
+
+        sear = browser.find_element_by_xpath('//*[@class="r"]/a')
+        sear = sear.get_attribute("href")
+
+        browser.quit()
+
+        page = requests.get(sear)
+        # print(page)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        # print(soup.get_text().encode('utf-8'))
+        extract = soup.select(".lyrics")
+        lyrics = (extract[0].get_text()).strip()
+
+        return lyrics
+
+    except Exception as e:
+        try:
+            # Method 2 musixmatch.com
+            options = webdriver.ChromeOptions()
+            options.add_argument('headless')
+            options.add_argument('window-size=1200x600')
+
+            browser = webdriver.Chrome(
+                r'C:\Users\Rishabh-Pc\Downloads\chromedriver', chrome_options=options)
+            mystr = song_name
+            # print(mystr)
+            s = "https://www.musixmatch.com/"
+            browser.get(s)
+            time.sleep(2)
+
+            cookies = browser.find_element_by_xpath(
+                '//*[@id="site"]/div/div[2]/div[1]/div[1]/button')
+            cookies.click()
+            time.sleep(1)
+
+            sbtn = browser.find_element_by_xpath(
+                '//*[@id="site"]/div/div[1]/div/main/div/div[1]/div[2]/div/div/div/div[1]/div/span/span/span/form/div/div[1]/input')
+            sbtn.send_keys(str(mystr))
+            time.sleep(3)
+
+            mysearch = browser.find_element_by_xpath(
+                '//*[@id="site"]/div/div[1]/div/main/div/div[1]/div[2]/div/div/div/div[1]/div/span/span/span[2]/div/div/div/div[1]/div[2]/div/ul/li/a/div[2]')
+            mysearch.click()
+            time.sleep(2)
+
+            # browser.get_screenshot_as_file('main-page.png')
+
+            ele = browser.find_elements_by_class_name('mxm-lyrics__content ')
+            lyrics = ''
+            for e in ele:
+                lyrics += e.text + "\n"
+
+            browser.quit()
+
+            if lyrics == '':
+                lyrics = ["We are not authorised to get the lyrics for this song."]
+
+            return lyrics
+
+        except Exception as e:
+            lyrics = ["No Lyrics found for this song."]
+            browser.quit()
+            return lyrics
+
+# Run this function for testing purposes
+# print(lyrics_generator("Let me go"))
